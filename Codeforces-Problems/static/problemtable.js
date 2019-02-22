2220a7f9c4fc65c5da60c7da70d13f79e9fe559b
Yang-33 <kasai.yuta0810@gmail.com>
2019-02-23 00:27:09 +0900

    update Sat Feb 23 00:27:09 DST 2019

--- Codeforces-Problems/static/problemtable.js
+++ Codeforces-Problems/static/problemtable.js
@@ -6,5 +6,52 @@ jQuery(function ($) {
                    "paging": false,
                     "order": [ [ 2, "desc" ] ],
                  "language": {"search": "Search:"}
-            });
+                 });
         });
+
+/* Custom filtering function which will search data in column four between two values */
+$.fn.dataTable.ext.search.push(
+    function( settings, data, dataIndex ) {
+        var min = parseInt( $('#mindiff').val(), 10 );
+        var max = parseInt( $('#maxdiff').val(), 10 );
+        var age = parseFloat( data[1] ) || 0; // use data for the age column
+        if ( ( isNaN( min ) && isNaN( max ) ) ||
+             ( isNaN( min ) && age <= max ) ||
+             ( min <= age   && isNaN( max ) ) ||
+             ( min <= age   && age <= max ) )
+        {
+            return true;
+        }
+        return false;
+    }
+);
+
+$.fn.dataTable.ext.search.push(
+    function( settings, data, dataIndex ) {
+        var min = parseInt( $('#minsolve').val(), 10 );
+        var max = parseInt( $('#maxsolve').val(), 10 );
+        var age = parseFloat( data[2] ) || 0; // use data for the age column
+        if ( ( isNaN( min ) && isNaN( max ) ) ||
+             ( isNaN( min ) && age <= max ) ||
+             ( min <= age   && isNaN( max ) ) ||
+             ( min <= age   && age <= max ) )
+        {
+            return true;
+        }
+        return false;
+    }
+); 
+
+$(document).ready(function() {
+    var table = $('#tablelist').DataTable();
+
+    // Event listener to the two range filtering inputs to redraw on input 1
+    $('#mindiff, #maxdiff').keyup( function() {
+        table.draw();
+    } );
+    // Event listener to the two range filtering inputs to redraw on input 2
+    $('#minsolve, #maxsolve').keyup( function() {
+        table.draw();
+    } );
+} );
+
