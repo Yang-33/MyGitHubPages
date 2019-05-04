c1067c08328a11e997da771614df74c2d3965387
Yang-33 <kasai.yuta0810@gmail.com>
2019-05-04 18:54:46 +0900

    update Sat May  4 18:54:46 DST 2019

--- Codeforces-Problems/static/problemtable.js
+++ Codeforces-Problems/static/problemtable.js
@@ -1,57 +1,223 @@
-jQuery(function ($) {
-            $("#tablelist").DataTable({
-              "lengthChange": false,
-               "lengthMenu": [[-1], ["All"]],
-                     "info": false,
-                   "paging": false,
-                    "order": [ [ 2, "desc" ] ],
-                 "language": {"search": "Search:"}
-                 });
-        });
-
-/* Custom filtering function which will search data in column four between two values */
-$.fn.dataTable.ext.search.push(
-    function( settings, data, dataIndex ) {
-        var min = parseInt( $('#mindiff').val(), 10 );
-        var max = parseInt( $('#maxdiff').val(), 10 );
-        var age = parseFloat( data[1] ) || 0; // use data for the age column
-        if ( ( isNaN( min ) && isNaN( max ) ) ||
-             ( isNaN( min ) && age <= max ) ||
-             ( min <= age   && isNaN( max ) ) ||
-             ( min <= age   && age <= max ) )
-        {
-            return true;
+document.getElementById("submitUsername").onclick = function() {
+	var username = document.getElementById("username")
+	changeBackColorDefault("td")
+	usernameProcess(username.value)
+};
+        function flipTagshowhide(boolvisible){
+            var elements = document.getElementsByClassName("problem-tag");
+            if (boolvisible){
+                for(i=0;i<elements.length;i++){
+                    elements[i].style.visibility="visible"
+                }
+            }else {
+                for(i=0;i<elements.length;i++){
+                    elements[i].style.visibility="hidden"
+                }
+            }
         }
-        return false;
-    }
-);
 
-$.fn.dataTable.ext.search.push(
-    function( settings, data, dataIndex ) {
-        var min = parseInt( $('#minsolve').val(), 10 );
-        var max = parseInt( $('#maxsolve').val(), 10 );
-        var age = parseFloat( data[2] ) || 0; // use data for the age column
-        if ( ( isNaN( min ) && isNaN( max ) ) ||
-             ( isNaN( min ) && age <= max ) ||
-             ( min <= age   && isNaN( max ) ) ||
-             ( min <= age   && age <= max ) )
-        {
+function changeBackColorDefault(classname){
+  var elements = document.getElementsByTagName(classname);
+  for(i=0;i<elements.length;i++){
+    elements[i].style.backgroundColor = "transparent"; // default color
+  }
+}      
+
+function changeBackColorAC(classname){
+  var elements = document.getElementsByClassName(classname);
+  for(i=0;i<elements.length;i++){
+    elements[i].style.backgroundColor = "#D5EAD8"; // accept color
+  }
+}
+function changeBackColorWA(classname){
+  var elements = document.getElementsByClassName(classname);
+  for(i=0;i<elements.length;i++){
+    elements[i].style.backgroundColor = "#F2DEDE"; // wrong answer color
+  }
+}
+function abledTags(classname){ // tag戻す
+  var elements = document.getElementsByClassName(classname);
+  for(i=0;i<elements.length;i++){
+    elements[i].style.visibility="visible"
+  }
+}
+function disabledTags(classname){ // tag消す
+  var elements = document.getElementsByClassName(classname);
+  for(i=0;i<elements.length;i++){
+    elements[i].style.visibility="hidden"
+  }
+}
+function usernameProcess(username){
+	var xmlHttpRequest = new XMLHttpRequest();
+	xmlHttpRequest.onreadystatechange = function()
+	{
+	    if( this.readyState == 4 && this.status == 200 )
+	    {
+	        if( this.response )
+	        {
+	            var usersub = (this.response["result"]);
+				
+				var ProblemState = {}
+				for(var i = 0 ; i < usersub.length ; i++){
+					var ProblemId = usersub[i]["problem"]["contestId"] + usersub[i]["problem"]["index"]
+					var Verdict = usersub[i]["verdict"]
+					if (Verdict === "OK"){
+						ProblemState[ProblemId] = "AC"
+	                }else {
+						if ((ProblemId in ProblemState)&&(ProblemState[ProblemId]==="AC"))continue;
+						// TODO: add other State
+						//ProblemState[ProblemId] = "??"
+						ProblemState[ProblemId] = "WA"
+						
+				
+	                }
+	            }
+				
+				var ProblemStateCnt = Object.keys(ProblemState).length
+
+				// 取ってこれたので色を🐸
+				for (var problemId in ProblemState){	
+					if (ProblemState[problemId]==="AC"){
+						changeBackColorAC(problemId)
+	                }else {
+	                	changeBackColorWA(problemId)
+					}
+				}
+	        }
+	    }
+	}
+
+	xmlHttpRequest.open( 'GET', 'https://codeforces.com/api/user.status?handle='+username, true );
+	xmlHttpRequest.responseType = 'json';
+	xmlHttpRequest.send( null );
+}
+
+function parseurl(){
+	var parser = new URL(location.href);
+	var username = parser.searchParams.get("user");
+	if (username !== "" && username !== null){
+		changeBackColorDefault("td");
+		usernameProcess(username);
+	}
+}
+function copyusername(){
+	var parser = new URL(location.href);
+	var username = parser.searchParams.get("user");
+	if (username !== "" && username !== null){
+		document.getElementById("username").value = username;
+	}
+}
+
+
+// TABLE
+var options = {
+  valueNames: [ 'problem-name', 'point', 'solvecount', 'tags' ]
+};
+var tablelist = new List('ptable', options);
+
+function filtertable(minpoint,maxpoint,minsolved,maxsolved){
+      tablelist.filter(
+        function(item){
+          var point_n = parseInt( item.values().point, 10) || 0;
+          var solved_n = parseInt( item.values().solvecount, 10);
+          
+          if (
+              (
+               ( isNaN( minpoint ) && isNaN( maxpoint ) ) ||
+               ( isNaN( minpoint ) && point_n <= maxpoint ) ||
+               ( minpoint <= point_n   && isNaN( maxpoint ) ) ||
+               ( minpoint <= point_n   && point_n <= maxpoint )
+              ) && (
+               ( isNaN( minsolved ) && isNaN( maxsolved ) ) ||
+               ( isNaN( minsolved ) && solved_n <= maxsolved ) ||
+               ( minsolved <= solved_n   && isNaN( maxsolved ) ) ||
+               ( minsolved <= solved_n   && solved_n <= maxsolved )
+              )
+             )
+          {
             return true;
+          }else{
+            return false;
+          }
         }
-        return false;
+      );
+}
+
+
+var headers = document.querySelectorAll('th.sort');
+for (var i = 0; i < headers.length; i++) {
+  headers[i].addEventListener('click', function (){
+    for (var j = 0; j < headers.length; j++) {
+      var classList = headers[j].classList;
+      classList.remove('mdl-data-table__header--sorted-ascending');
+      classList.remove('mdl-data-table__header--sorted-descending');
+      if (classList.contains('asc')) {
+        classList.add('mdl-data-table__header--sorted-ascending');
+      } else if (classList.contains('desc')) {
+        classList.add('mdl-data-table__header--sorted-descending');
+      }
     }
-); 
+  });
+}
 
-$(document).ready(function() {
-    var table = $('#tablelist').DataTable();
 
-    // Event listener to the two range filtering inputs to redraw on input 1
-    $('#mindiff, #maxdiff').keyup( function() {
-        table.draw();
-    } );
-    // Event listener to the two range filtering inputs to redraw on input 2
-    $('#minsolve, #maxsolve').keyup( function() {
-        table.draw();
-    } );
-} );
 
+// CODE IS HERE
+function parseurl_uku(){
+	var parser = new URL(location.href);
+	var username = parser.searchParams.get("user");
+	var minpoint = parser.searchParams.get("mindifficulty");
+	var maxpoint = parser.searchParams.get("maxdifficulty");
+	var minsolved = parser.searchParams.get("minsolved");
+	var maxsolved = parser.searchParams.get("maxsolved");
+	if (username !== "" && username !== null){
+		changeBackColorDefault("td");
+		usernameProcess(username);
+	}
+	if (minpoint === "" || minpoint === null){
+		minpoint = NaN;
+	}
+	if (maxpoint === "" || maxpoint === null){
+		maxpoint = NaN;
+	}
+	if (minsolved === "" || minsolved === null){
+		minsolved = NaN;
+	}
+	if (maxsolved === "" || maxsolved === null){
+		maxsolved = NaN;
+	}
+	filtertable(minpoint,maxpoint,minsolved,maxsolved);
+	
+}
+function copyusername_uku(){
+	var parser = new URL(location.href);
+	var username = parser.searchParams.get("user");
+	if (username !== "" && username !== null){
+		document.getElementById("username").value = username;
+	}
+	var minpoint = parser.searchParams.get("mindifficulty");
+	if (minpoint !== "" && minpoint !== null){
+		document.getElementById("mindiff").value = minpoint;
+	}
+	var maxpoint = parser.searchParams.get("maxdifficulty");
+	if (maxpoint !== "" && maxpoint !== null){
+		document.getElementById("maxdiff").value = maxpoint;
+	}
+	var minsolved = parser.searchParams.get("minsolved");
+	if (minsolved !== "" && minsolved !== null){
+		document.getElementById("minsolve").value = minsolved;
+	}
+	var maxsolved = parser.searchParams.get("maxsolved");
+	if (maxsolved !== "" && maxsolved !== null){
+		document.getElementById("maxsolve").value = maxsolved;
+	}
+	
+}
+
+
+copyusername_uku();
+parseurl_uku();
+
+// ダメダメ
+headers[2].click();
+headers[2].click();
